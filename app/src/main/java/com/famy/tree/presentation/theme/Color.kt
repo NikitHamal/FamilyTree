22c40b48980ce1f2cd1823567ae8eb7298c28ea1
Nikit Hamal <iamnikithamal@gmail.com>
2026-04-26 17:26:23 +0545

    up

--- app/src/main/java/com/famy/tree/presentation/theme/Color.kt
+++
@@ -1,9 +0,0 @@
-package com.famy.tree.presentation.theme
-
-import androidx.compose.ui.graphics.Color
-
-val FamySeed = Color(0xFF396A5F)
-val FamyPaternal = Color(0xFF2E6F9E)
-val FamyMaternal = Color(0xFF9D5C8F)
-val FamyLeaf = Color(0xFF7FB99A)
-val FamyEarth = Color(0xFF8A6F4D)
